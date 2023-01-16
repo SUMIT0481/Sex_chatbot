fc1492cb2ccb4793be50f54356b7e4d19504f84a
SUMIT0481 <118965746+SUMIT0481@users.noreply.github.com>
2023-01-16 18:06:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config(object):
-      BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-      OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "")      
+      BOT_TOKEN = os.environ.get("BOT_TOKEN", "5834853771:AAGjdMnpauHLxkAc3Bs6xQdRT95ehdoxge8")
+      OWNER_USERNAME = os.environ.get("OWNER_USERNAME", "ITZ_ME_YOUR_VENOM")      
